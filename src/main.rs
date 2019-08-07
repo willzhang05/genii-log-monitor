68cbcb876e2ae1abe34e4b20acc4dad158c5712e
William Zhang <willzhang05@gmail.com>
2019-08-07 19:48:28 +0000

    add testing write code

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+extern crate chrono;
+
 use std::env;
 use std::thread;
 use std::fs::File;
 use std::io::{BufReader, Read};
 use std::io::prelude::*;
 use std::sync::mpsc::channel;
+//use notify::{RecommendedWatcher, RecursiveMode, Result, watcher};
+use std::time::Duration;
+use chrono::prelude::*;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 3 {
         if &args[1] == "start" {
+            
+            //let mut watcher = watcher(Duration::from_secs(1)).expect("Failed to create watcher.");
+            //watcher.watch(&args[2]).expect("Failed to watch file.");
 
             let (tx, rx) = channel();
 
@@ -20,6 +28,7 @@ fn main() {
                 let mut contents = String::new();
                 loop {
                     let line_len = buf_reader.read_line(&mut contents).expect("Unable to read from buffer.");
+                    buf_reader.consume(line_len);
                     println!("{}", contents);
                     tx.send(contents.to_owned()).expect("Unable to send on channel");
                 }
@@ -32,6 +41,15 @@ fn main() {
 
             reader.join().expect("The sender thread has panicked!");
             notifier.join().expect("the receiver thread has panicked!");
+        } else if &args[1] == "test" {
+            let mut buffer = File::create(&args[2]).expect("Unable to create file.");
+            loop {
+                let local: DateTime<Local> = Local::now();
+                let local_time = local.to_string() + "\n";
+                let bytes_written = buffer.write(&local_time.as_bytes()).expect("Unable to write data.");
+                println!("{:?}", local);
+                thread::sleep(Duration::from_millis(1000));
+            }
         }
     } else {
         eprintln!("Invalid arguments");
--- write.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-while true;
-do
-	date >> test.txt
-	sleep 1
-done
