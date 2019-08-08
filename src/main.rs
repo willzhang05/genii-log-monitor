03cfd2a9ed4b691e03c34b8096146f5a5b254c75
William Zhang <willzhang05@gmail.com>
2019-08-08 16:54:05 +0000

    add enabling of container monitoring

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,8 @@ struct Container {
     name: String,
     install_dir: String,
     log_file: String,
-    properties_file: String
+    properties_file: String,
+    enabled: bool
 }
 
 #[derive(serde::Deserialize)]
@@ -36,34 +37,36 @@ fn main() {
             let config_file = std::fs::read_to_string(&args[2]).expect("Unable to open config file.");
             let config_info: Config = toml::from_str(&config_file).expect("Unable to parse config file.");
 
-
             for container in config_info.containers {
-                let (tx, rx) = mpsc::channel();
+                if container.enabled {
+                    let (tx, rx) = mpsc::channel();
 
-                let reader = thread::spawn(move || {
-                    let log_path = Path::new(&container.install_dir).join(&container.log_file);
-                    let file = File::open(&log_path).expect("Unable to open file.");
-                    let mut f = BufReader::new(file);
-                    let mut contents =  String::new();
-                    loop {
-                        contents.clear();
-                        let line_len = f.read_line(&mut contents).expect("Unable to read line.");
-                        f.consume(line_len);
-                        if contents.len() != 0 {
-                            tx.send(contents.to_owned()).expect("Unable to send on channel");
+                    let reader = thread::spawn(move || {
+                        let log_path = Path::new(&container.install_dir).join(&container.log_file);
+                        println!("{:?}", log_path);
+                        let file = File::open(&log_path).expect("Unable to open file.");
+                        let mut f = BufReader::new(file);
+                        let mut contents =  String::new();
+                        loop {
+                            contents.clear();
+                            let line_len = f.read_line(&mut contents).expect("Unable to read line.");
+                            f.consume(line_len);
+                            if contents.len() != 0 {
+                                tx.send(contents.to_owned()).expect("Unable to send on channel");
+                            }
                         }
-                    }
-                });
+                    });
 
-                let notifier = thread::spawn(move || {
-                    loop {
-                        let value = rx.recv().expect("Unable to receive from channel");
-                        println!("{}", value);
-                    }
-                });
+                    let notifier = thread::spawn(move || {
+                        loop {
+                            let value = rx.recv().expect("Unable to receive from channel");
+                            println!("{}", value);
+                        }
+                    });
 
-                reader.join().expect("The sender thread has panicked!");
-                notifier.join().expect("the receiver thread has panicked!");
+                    reader.join().expect("The sender thread has panicked!");
+                    notifier.join().expect("the receiver thread has panicked!");
+                }
             }
 
         } else if &args[1] == "test" {
