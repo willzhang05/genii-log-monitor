9a151e2ba0267c5d423a127b32de4fe9851ba583
William Zhang <willzhang05@gmail.com>
2019-08-08 21:01:31 +0000

    working read properties file

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,13 @@ extern crate toml;
 extern crate chrono;
 
 use std::string::String;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::env;
 use std::thread;
-use std::fs::File;
+use std::fs;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::sync::mpsc;
-//use toml::{Value, de::Error};
 
 use std::time::Duration;
 use chrono::prelude::*;
@@ -18,7 +17,6 @@ use chrono::prelude::*;
 struct Container {
     name: String,
     install_dir: String,
-    log_file: String,
     properties_file: String,
     enabled: bool
 }
@@ -28,23 +26,52 @@ struct Config {
     containers: Vec<Container>
 }
 
+fn read_log_properties(prop_path: &Path) -> (PathBuf, usize) {
+    let prop_file = fs::File::open(prop_path).expect("Unable to open properties file.");
+
+    let mut prop_reader = BufReader::new(prop_file);
+    let prop_contents = String::new();
+    let mut log_path = PathBuf::new();
+    let mut max_log_size: usize = 0;
+
+    for line in prop_reader.lines() {
+        let line_str = line.unwrap();
+        if line_str.starts_with('#') {
+            continue;
+        }
+        if line_str.contains("log4j.appender.LOGFILE.File") {
+            let split_line: Vec<&str> = line_str.split("=").to_owned().collect();
+            log_path = Path::new(split_line[1].clone()).to_path_buf();
+        }
+        if line_str.contains("log4j.appender.LOGFILE.MaxFileSize") {
+            let split_line: Vec<&str> = line_str.split("=").to_owned().collect();
+            max_log_size = split_line[1].clone().parse::<usize>().unwrap();
+        }
+    }
+    return (log_path, max_log_size);
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 3 {
         if &args[1] == "start" {
 
-            let config_file = std::fs::read_to_string(&args[2]).expect("Unable to open config file.");
+            let config_file = fs::read_to_string(&args[2]).expect("Unable to open config file.");
             let config_info: Config = toml::from_str(&config_file).expect("Unable to parse config file.");
 
             for container in config_info.containers {
                 if container.enabled {
+
+                    let prop_path = Path::new(&container.install_dir).join(&container.properties_file);
+                    let (log_path, max_log_size) = read_log_properties(&prop_path);
+
                     let (tx, rx) = mpsc::channel();
 
                     let reader = thread::spawn(move || {
-                        let log_path = Path::new(&container.install_dir).join(&container.log_file);
+                        //let log_path = Path::new(&container.install_dir).join(&container.log_file);
                         println!("{:?}", log_path);
-                        let file = File::open(&log_path).expect("Unable to open file.");
+                        let file = fs::File::open(&log_path).expect("Unable to open log file.");
                         let mut f = BufReader::new(file);
                         let mut contents =  String::new();
                         loop {
@@ -52,14 +79,14 @@ fn main() {
                             let line_len = f.read_line(&mut contents).expect("Unable to read line.");
                             f.consume(line_len);
                             if contents.len() != 0 {
-                                tx.send(contents.to_owned()).expect("Unable to send on channel");
+                                tx.send(contents.to_owned()).expect("Unable to send on channel.");
                             }
                         }
                     });
 
                     let notifier = thread::spawn(move || {
                         loop {
-                            let value = rx.recv().expect("Unable to receive from channel");
+                            let value = rx.recv().expect("Unable to receive from channel.");
                             println!("{}", value);
                         }
                     });
@@ -70,7 +97,7 @@ fn main() {
             }
 
         } else if &args[1] == "test" {
-            let mut buffer = File::create(&args[2]).expect("Unable to create file.");
+            let mut buffer = fs::File::create(&args[2]).expect("Unable to create file.");
             loop {
                 let local: DateTime<Local> = Local::now();
                 let local_time = local.to_string() + "\n";
@@ -80,6 +107,6 @@ fn main() {
             }
         }
     } else {
-        eprintln!("Invalid arguments");
+        eprintln!("Invalid arguments.");
     }
 }
